444a05939cdfee94da9411eb12c8e1a161b692f9
Jordan Maynor <mail@jtm.li>
2024-08-19 13:39:33 -0500

    Added some basics

--- src/main.py
+++ src/main.py
@@ -4,14 +4,21 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
+import threading
+
+from src.create_opml import create_opml, get_subscriptions
+from src.serve_opml import serve_opml
 
 if __name__ == "__main__":
     """
     Main entry point of program
     """
 
-    # First check if OPML file exists
+    # Create OPML
+    subscriptions = get_subscriptions()
+    create_opml(subscriptions)
 
-    # If not, create it
-
-    # Serve the OPML file
+    # Serve OPML
+    thread = threading.Thread(target=serve_opml)
+    thread.start()
+    thread.join()
--- src/serve_opml.py
+++ src/serve_opml.py
@@ -1,15 +1,15 @@
 import http.server
 import os
 import socketserver
-import threading
 
 
 def serve_opml():
     """
-    Serves OPML file over HTTP server
+    Serves the .opml file over HTTP server
     """
-    handler = http.server.SimpleHTTPRequestHandler
 
-    with socketserver.TCPServer(("", int(os.environ["HTTP_PORT"])), handler) as httpd:
-        print(f"Server started on port {os.environ['HTTP_PORT']}")
+    Handler = http.server.SimpleHTTPRequestHandler
+
+    with socketserver.TCPServer(("", int(os.environ["HTTP_PORT"])), Handler) as httpd:
+        print(f"Serving at port {os.environ['HTTP_PORT']}")
         httpd.serve_forever()
